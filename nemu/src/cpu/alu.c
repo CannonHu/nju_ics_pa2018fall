a80ff6634a092fc5ca046871a11305d8827deffa
tracer-pa2018-fall <tracer@njuics.org>
2018-09-14 20:34:23 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:34:23 up 10:01,  1 user,  load average: 0.00, 0.00, 0.00 6aabdb5fc4affa315c72a14efe94b9406bdfbc70

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -6,6 +6,15 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 	cpu.eflags.CF = result<src;
 }
 
+void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	if(src != 0x00000000)
+		cpu.eflags.CF = result<=src;
+	else
+		cpu.eflags.CF = 0;
+}
+
 void set_ZF(uint32_t result,size_t data_size){
 	result = result & (0xFFFFFFFF >> (32 - data_size));
 	cpu.eflags.ZF=(result==0);
@@ -75,9 +84,15 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest + src + cpu.eflags.CF;
+	
+	set_CF_adc(res,src,data_size);
+	set_PF(res);
+
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+	set_OF_add(res,src,dest,data_size);
 #endif
 }
 
