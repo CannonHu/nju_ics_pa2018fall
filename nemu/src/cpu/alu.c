56010f49bc4e9af280ac62357bb80cf7cb567947
tracer-pa2018-fall <tracer@njuics.org>
2018-09-14 23:44:46 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  23:44:46 up 13:11,  1 user,  load average: 0.00, 0.01, 0.00 963ec9bfebdb0e1921746ee332c435d65c203e06

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -75,6 +75,30 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 	}
 }
 
+void set_OF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+	switch(data_size){
+		case 8:
+			result = sign_ext(result & 0xFF,8);
+			src = sign_ext(src & 0xFF,8);
+			dest = sign_ext(dest & 0xFF,8);
+			break;
+		case 16:
+			result = sign_ext(result & 0xFFFF,16);
+			src = sign_ext(src & 0xFFFF,16);
+			dest = sign_ext(dest & 0xFFFF,16);
+			break;
+		default:break;
+	}
+	if(sign(src) != sign(dest)){
+		if(sign(dest) != sign(result))
+			cpu.eflags.OF = 1;
+		else
+			cpu.eflags.OF = 0;
+	}
+	else{
+		cpu.eflags.OF = 0;
+	}
+}
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
