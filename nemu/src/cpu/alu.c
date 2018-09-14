cfe78d0a25e9588f67a6d8a5bd7a0273d09f57cb
tracer-pa2018-fall <tracer@njuics.org>
2018-09-14 21:31:33 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:31:33 up 10:58,  1 user,  load average: 0.00, 0.00, 0.00 ebbfff0251a2e054b555eab6de99de38620e295c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -14,7 +14,7 @@ void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)),data_size);
-	if(cpu.dflags.CF == 1){
+	if(cpu.eflags.CF == 1){
 		if(src != 0x00000000 && dest != 0x00000000)
 			cpu.eflags.CF = result<=src;
 		else
