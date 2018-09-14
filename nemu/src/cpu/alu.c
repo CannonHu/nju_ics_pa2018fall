82084aca8d37364ea572102a4a2e60714e6f2afc
tracer-pa2018-fall <tracer@njuics.org>
2018-09-14 20:57:24 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:57:24 up 10:24,  1 user,  load average: 0.00, 0.02, 0.01 9b6a3385e5b2992f84e8be14d6468cb85fb29e4d

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -4,6 +4,8 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	cpu.eflags.CF = result<src;
+	printf(" CF: %x\n",cpu.eflags.CF);
+
 }
 
 void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
@@ -72,6 +74,7 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size) {
 #else
 	uint32_t res = 0;
 	res = dest + src;
+	printf("src: %x dest: %x\n",src,dest);
 
 	set_CF_add(res,src,data_size);
 	set_PF(res);
