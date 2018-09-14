972be737dd4932afafe1031f7ae4d42de8f5ab98
tracer-pa2018-fall <tracer@njuics.org>
2018-09-14 22:27:42 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:27:42 up 11:54,  1 user,  load average: 0.00, 0.00, 0.00 7a85050632e6257cdf19e5f4283b0390af583aba

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -4,12 +4,11 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	cpu.eflags.CF = result<src;
-	printf(" CF: %d\n",cpu.eflags.CF);
-
+	
 }
 
 void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
-	printf(" CF: %d",cpu.eflags.CF);
+
 	if((src == 0x00000000 || dest == 0x00000000) && cpu.eflags.CF == 0){
 		cpu.eflags.CF = 0;
 		return;
@@ -21,7 +20,6 @@ void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 		cpu.eflags.CF=result<=src;
 	else
 		cpu.eflags.CF=result<src;
-	printf(" CF: %d\n",cpu.eflags.CF);
 }
 
 void set_ZF(uint32_t result,size_t data_size){
@@ -96,7 +94,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size) {
 #else
 	uint32_t res = 0;
 	res = dest + src + cpu.eflags.CF;
-	printf("src: %x dest: %x\n",src,dest);
+	
 	set_CF_adc(res,src,dest,data_size);
 	set_PF(res);
 
@@ -112,9 +110,17 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sub(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	uint32_t src_t = ~src + 1;
+	res = dest + src_t;
+
+	set_CF_add(res,src,data_size);
+	set_PF(res);
+
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+	set_OF_add(res,src,dest,data_size);
+	return res & (0xFFFFFFFF >> (32-data_size));
 #endif
 }
 
