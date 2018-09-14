4b944b88ca860be0a1b37c763f0eb2d209f67c5c
monitor <tracer@njuics.org>
2018-09-14 21:20:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -8,12 +8,12 @@ void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 
 }
 
-void set_CF_adc(uint32_t result,uint32_t src,size_t data_size){
+void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 	printf(" CF: %x",cpu.eflags.CF);
 
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
-	if(src != 0x00000000)
+	if(src != 0x00000000 && dest != 0x00000000)
 		cpu.eflags.CF = result<=src;
 	else
 		cpu.eflags.CF = 0;
