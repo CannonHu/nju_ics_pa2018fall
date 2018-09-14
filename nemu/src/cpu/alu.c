a01b35170abbb4e2d25881ebcc723e1eb9d089ed
monitor <tracer@njuics.org>
2018-09-14 23:35:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -3,9 +3,7 @@
 void set_CF_add(uint32_t result,uint32_t src,size_t data_size){
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
-	cpu.eflags.CF = result<src;
-	printf("CF: %d",cpu.eflags.CF);
-	
+	cpu.eflags.CF = result<src;	
 }
 
 void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
@@ -23,7 +21,13 @@ void set_CF_adc(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 		cpu.eflags.CF=result<src;
 }
 
+void set_CF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	cpu.eflags.CF=result>src;
 
+}
 void set_ZF(uint32_t result,size_t data_size){
 	result = result & (0xFFFFFFFF >> (32 - data_size));
 	cpu.eflags.ZF=(result==0);
