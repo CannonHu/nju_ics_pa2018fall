48b7050d823ee6cbfde820d34b674ac0029cd08b
monitor <tracer@njuics.org>
2018-09-15 20:49:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -213,10 +213,7 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size) {
 	printf("%d %x %x\n",data_size,dest,src);
 	printf("%llx %llx\n",(int64_t)dest,(int64_t)src);
 	res = (int64_t)dest * (int64_t)src;
-	printf("%d\n",(int32_t)__ref_alu_imul(src,dest,data_size));
-	printf("%d\n",(int32_t)res);
-
-	return res & (0xFFFFFFFFFFFFFFFF >> (64 - data_size * 2));
+	printf("%4 - data_size * 2));
 #endif
 }
 
