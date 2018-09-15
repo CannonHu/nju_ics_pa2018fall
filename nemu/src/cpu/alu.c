5dd3c1812f7e2c59f945e9a81ed3c84f94194628
tracer-pa2018-fall <tracer@njuics.org>
2018-09-15 10:06:52 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:06:52 up 13:41,  1 user,  load average: 0.18, 0.14, 0.09 f17404ff8f86726fcd6186ccc4f754a241acd8f

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -28,6 +28,19 @@ void set_CF_sub(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 	cpu.eflags.CF=result>dest;
 
 }
+
+void set_CF_sbb(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
+
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)),data_size);
+	if(cpu.eflags.CF == 1)
+		cpu.eflags.CF=result>=dest;
+	else
+		cpu.eflags.CF=result>dest;
+}
+
+
 void set_ZF(uint32_t result,size_t data_size){
 	result = result & (0xFFFFFFFF >> (32 - data_size));
 	cpu.eflags.ZF=(result==0);
@@ -157,9 +170,17 @@ uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_sbb(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	uint32_t res = 0;
+	res = dest - src - cpu.eflags.CF;
+	
+	set_CF_sbb(res,src,dest,data_size);
+	set_PF(res);
+
+	set_ZF(res,data_size);
+	set_SF(res,data_size);
+	set_OF_sub(res,src,dest,data_size);
+	return res & (0xFFFFFFFF >> (32-data_size));
+
 #endif
 }
 
