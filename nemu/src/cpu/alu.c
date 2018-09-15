a3b9226dafdbd7af87edeb63dd225472a4f421ca
tracer-pa2018-fall <tracer@njuics.org>
2018-09-15 13:56:51 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:56:51 up 17:31,  1 user,  load average: 0.06, 0.06, 0.04 b3e338bce95dcfd0f5faea0dcfb7b3282ea72058

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -191,10 +191,9 @@ uint64_t alu_mul(uint32_t src, uint32_t dest, size_t data_size) {
 #else
 	uint64_t res = 0;
 	printf("%x %x %x\n",data_size,dest,src);
-	int tl = 32 - (int)data_size;
-	printf("%x ",tl);
-	dest = dest >> tl;
-	src = src >> tl;
+
+	dest = dest & (0xFFFFFFFF >> (32-data_size));
+	src = src & (0xFFFFFFFF >> (32-data_size));
 	printf("%x %x\n",dest,src);
 	res = (uint64_t)dest * (uint64_t)src;
 	uint64_t t = res & (0xFFFFFFFFFFFFFFF >> (64 - data_size * 2));
