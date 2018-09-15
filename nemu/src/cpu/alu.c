4442b457e3eec751d09a3ba3e634236fdf53960a
tracer-pa2018-fall <tracer@njuics.org>
2018-09-15 11:39:41 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  11:39:41 up 15:14,  1 user,  load average: 0.00, 0.00, 0.04 625ce62979ec9edd9d56cb93b2f4bde6cd1bd8e7

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -333,10 +333,8 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size) {
 	return __ref_alu_sar(src, dest, data_size);	
 #else
 	uint32_t res = 0;	
-	printf("%d %x %x\n",data_size,dest,src);
-	dest = (dest & (0xFFFFFFFF >> (32-data_size))) << (32-data_size) ;
-	res = (int)dest >> (int)src;
-	printf("%x\n", res);
+	dest = dest & (0xFFFFFFFF >> (32-data_size));
+	res = (int)(dest << (32-data_size)) >> (int)src;
 	cpu.eflags.CF = dest >> (src-1);
 	set_PF(res);
 
