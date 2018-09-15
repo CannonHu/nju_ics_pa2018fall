b49d262d29d0ed8d5801e7834bf4b9745d0a909e
tracer-pa2018-fall <tracer@njuics.org>
2018-09-15 21:29:59 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:29:59 up 1 day,  1:04,  1 user,  load average: 0.11, 0.07, 0.02 f72c19546b12da491b04be93fef909e16cf11fb1

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -232,9 +232,10 @@ int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_idiv(src, dest, data_size);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	int32_t res = 0;
+	assert(src != 0);
+	res = dest / src;
+	return res;
 #endif
 }
 
@@ -254,9 +255,11 @@ int32_t alu_imod(int64_t src, int64_t dest) {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_imod(src, dest);
 #else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
+	int32_t res = 0;
+	assert(src != 0);
+	int32_t tmp = dest / src;
+	res = dest - tmp * src;
+	return res;
 #endif
 }
 
