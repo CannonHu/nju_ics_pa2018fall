d8326dde7cf822bfdbd913d5876cd7bc79f4e011
monitor <tracer@njuics.org>
2018-09-16 20:56:01 +0800

    monitor test

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -165,9 +165,9 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size) {
 
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
-	__ref_alu_sub(src,dest,data_size);
+	//__ref_alu_sub(src,dest,data_size);
 	set_OF_sub(res,src,dest,data_size);
-	//set_OF_add(res,src_t,dest,data_size);
+	set_OF_add(res,src_t,dest,data_size);
 	return res & (0xFFFFFFFF >> (32-data_size));
 #endif
 }
