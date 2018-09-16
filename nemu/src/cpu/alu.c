94329945e93f6d952aa1baca5f37ba8e4082e22d
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 20:40:35 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:40:35 up 1 day,  9:34,  1 user,  load average: 0.11, 0.05, 0.01 d36cc95a62cd9a64e71698b8a51400afe4931e36

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -77,6 +77,8 @@ void set_OF_add(uint32_t result,uint32_t src,uint32_t dest,size_t data_size){
 			break;
 		default:break;
 	}
+	printf("%d %x %x %x %d\n",data_size,dest,src,result,cpu.eflags.OF);
+
 	if(sign(src) == sign(dest)){
 		if(sign(src) != sign(result))
 			cpu.eflags.OF = 1;
