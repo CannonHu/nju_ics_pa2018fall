06a9e507cf08947ae1097211e68be36ef13bfce2
tracer-pa2018-fall <tracer@njuics.org>
2018-09-16 19:36:38 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:36:38 up 1 day,  9:13,  1 user,  load average: 0.07, 0.03, 0.01 ebe69af82f6599534d1f634d12cb5fea94f27870

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -161,6 +161,8 @@ uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size) {
 
 	set_ZF(res,data_size);
 	set_SF(res,data_size);
+	__ref_alu_sub(src,dest,data_size);
+	printf("%x %x %d\n",dest,src,cpu.eflags.OF);
 	set_OF_add(res,src_t,dest,data_size);
 	return res & (0xFFFFFFFF >> (32-data_size));
 #endif
