4cdb75eeb0a128bd6e8b6f260e6aa9f6256de02d
tracer-pa2018-fall <tracer@njuics.org>
2018-09-17 20:46:18 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  20:46:18 up 1 day, 19:47,  1 user,  load average: 0.15, 0.03, 0.01 d2e911fd5fc8cc902e9ef923c32887c7a7bd80ec

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -272,8 +272,7 @@ uint32_t internal_float_mul(uint32_t b, uint32_t a) {
 	uint32_t exp_res = 0;
 
 	/* TODO: exp_res = ? leave space for GRS bits. */
-	exp_res = fa.exponent + fb.exponent;
-	sig_res <<= 3;
+	exo_res = fa.exponent + fb.exponent - 127 - 20;
 
 	return internal_normalize(f.sign, exp_res, sig_res);
 }
