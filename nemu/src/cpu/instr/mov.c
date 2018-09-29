18f8902903922961c940ca5c5824e35436cee82f
monitor <tracer@njuics.org>
2018-09-29 15:31:01 +0800

    monitor test

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -6,18 +6,18 @@ static void instr_execute_2op() {
 	operand_write(&opr_dest);
 }
 
-make_instr_impl_2op(mov, r, rm, b){}
-make_instr_impl_2op(mov, r, rm, v){}
-make_instr_impl_2op(mov, rm, r, b){}
-make_instr_impl_2op(mov, rm, r, v){}
-make_instr_impl_2op(mov, i, rm, b){}
-make_instr_impl_2op(mov, i, rm, v){}
-make_instr_impl_2op(mov, i, r, b){}
-make_instr_impl_2op(mov, i, r, v){}
-make_instr_impl_2op(mov, a, o, b){}
-make_instr_impl_2op(mov, a, o, v){}
-make_instr_impl_2op(mov, o, a, b){}
-make_instr_impl_2op(mov, o, a, v){}
+make_instr_impl_2op(mov, r, rm, b)
+make_instr_impl_2op(mov, r, rm, v)
+make_instr_impl_2op(mov, rm, r, b)
+make_instr_impl_2op(mov, rm, r, v)
+make_instr_impl_2op(mov, i, rm, b)
+make_instr_impl_2op(mov, i, rm, v)
+make_instr_impl_2op(mov, i, r, b)
+make_instr_impl_2op(mov, i, r, v)
+make_instr_impl_2op(mov, a, o, b)
+make_instr_impl_2op(mov, a, o, v)
+make_instr_impl_2op(mov, o, a, b)
+make_instr_impl_2op(mov, o, a, v)
 
 
 make_instr_func(mov_r2rm_b) {
