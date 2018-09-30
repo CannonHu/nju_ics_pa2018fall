74436a9b1e9bdf0a9078fc93ab4dee636a90e44e
tracer-pa2018-fall <tracer@njuics.org>
2018-09-30 10:49:26 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:49:26 up 1 day,  3:31,  1 user,  load average: 1.18, 0.97, 0.86 cba0f905b9778a93dec9de026c3146e182f05816

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -24,8 +24,8 @@ extern uint8_t data_size;
 
 /* TODO: add more instructions here */
 make_instr_func(sub_i2rm_bv){	
-	print_asm_1("run");
 	int len = 1;
+	print_asm_2("run",len);
 	return len;
 }
 ;
