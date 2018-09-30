3233ed145da7dd89659706b567e0a4e4cf8c4dae
tracer-pa2018-fall <tracer@njuics.org>
2018-09-30 10:55:53 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  10:55:53 up 1 day,  3:38,  1 user,  load average: 1.02, 1.00, 0.92 b71c9dc3f956542c225c138fe1c549836299eb6d

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -23,36 +23,5 @@ extern uint8_t data_size;
 #include "cpu/instr/x87.h"
 
 /* TODO: add more instructions here */
-make_instr_func(sub_i2rm_bv){
-	printf("run\n");
-	OPERAND rm, imm;
-	rm.data_size = data_size;
-	imm.data_size = 8;
 
-	int len = 1;
-	printf("eip: %x",eip);
-	len += modrm_rm(eip, &rm);
-
-	imm.type = OPR_IMM;
-	imm.addr = eip + len;
-	operand_read(&imm);
-	imm.data_size = data_size;
-	imm.val = sign_ext(imm.val, 8);
-	rm.val = imm.val;
-	operand_write(&rm);
-	return len + 1;
-
-}
-make_instr_func(add_r2rm_b){
-	OPERAND r, rm;
-	r.data_size = 8;
-	rm.data_size = 8;
-	int len = 1;
-	len += modrm_r_rm(eip + 1, &r, &rm);
-	operand_read(&r);
-	rm.val = alu_add(r.val, rm.val, 8);
-	operand_write(&rm);
-	return len;
-
-}
 #endif
