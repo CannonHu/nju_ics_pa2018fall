adad29bde6d071768ed65aa145173162f0b93ef6
tracer-pa2018-fall <tracer@njuics.org>
2018-09-30 18:11:47 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  18:11:47 up 1 day,  5:53,  1 user,  load average: 0.14, 0.06, 0.07 add4b262f28e93471e24bbf65a9c2038aaf8e85b

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -1,11 +1,18 @@
 #include "cpu/instr.h"
 
 make_instr_func(sub_i2rm_bv){
-	OPERAND rm;
+	OPERAND rm, imm;
 	rm.data_size = data_size;
+	imm.data_size = 8;
 
 	int len = 1;
 	len += modrm_rm(eip + 1,&rm);
-
+	
+	imm.type = OPR_IMM;
+	imm.addr = eip + len;
+	operand_read(&imm);
+	uint32_t tval = sign_ext(imm.val, 8);
+	rm.val = alu_sub(tval, rm.val, data_size);
+	operand_write(&rm);
 	return len + 1;
 }
