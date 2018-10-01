fa8d702fcdcf16af8f0d30a38c1b0a52111bb458
monitor <tracer@njuics.org>
2018-10-01 19:56:01 +0800

    monitor test

--- nemu/src/cpu/instr/alu_instr.c
+++ nemu/src/cpu/instr/alu_instr.c
@@ -1,3 +1,9 @@
 #include "cpu/instr.h"
 
+static void instr_execute_2op(){
+	operand_read(&opr_src);
+	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
 
+make_instr_impl_2op(add, i, rm, b)
