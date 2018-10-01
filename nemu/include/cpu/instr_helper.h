177bf659260ee52036a856c9f3b11dd18af8c63b
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 21:04:45 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:04:45 up 1 day, 13:49,  1 user,  load average: 0.03, 0.12, 0.09 dedd65a58eb158f097abef61b6dbc6a08b1a2bca

--- nemu/include/cpu/instr_helper.h
+++ nemu/include/cpu/instr_helper.h
@@ -159,7 +159,7 @@ void print_asm_3(char * instr, char * suffix, uint8_t len, OPERAND * opr_1, OPER
 static inline bool inv_cc();
 
 #define condition_e \
-	cpu.eflags.ZF == 1;\
+	(cpu.eflags.ZF == 1);\
 
 
 #define condition_a \
