9f5a6a135f2c32767f19c72fa99b6c4fc4cf0c8c
monitor <tracer@njuics.org>
2018-10-01 20:26:01 +0800

    monitor test

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -95,3 +95,6 @@ static void instr_execute_2op(){
 	operand_read(&opr_dest);
 	alu_and(opr_src.val, opr_dest.val, data_size);
 }
+
+make_instr_impl_2op(test,r,rm,b)
+make_instr_impl_2op(test,r,rm,v)
