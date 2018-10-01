d80f25254e4848186c067e87f6bae55eef0e54eb
tracer-pa2018-fall <tracer@njuics.org>
2018-10-01 17:38:20 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:38:20 up 1 day, 11:19,  1 user,  load average: 0.40, 0.18, 0.07 2076809463d967d2a651409c38692e89284a175c

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -69,7 +69,7 @@ make_instr_func(call_near){
 	dest.data_size = data_size;
 	dest.type = OPR_MEM;
 	dest.addr = cpu.esp;
-	dest.val = eipval;
+	dest.val = eipval + data_size / 8;
 	operand_write(&dest);
 
 	addr.data_size = data_size;
