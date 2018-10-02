1f9abbb2926332ab4fa6c4bd4a97c73f392bafb6
tracer-pa2018-fall <tracer@njuics.org>
2018-10-02 21:49:21 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:49:21 up 2 days,  3:22,  1 user,  load average: 0.23, 0.08, 0.02 11e89f074e3ff9c6d23ff7a4b51d7b89476da3b3

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -10,3 +10,21 @@ make_instr_impl_2op(add, r, rm, v)
 make_instr_impl_2op(add, i, rm, v)
 make_instr_impl_2op(add, i, a, v)
 
+
+make_instr_func(add_i2rm_bv){
+	OPERAND rm, imm;
+	rm.data_size = imm.data_size = data_size;
+
+	int len = 1;
+	len += modrm_rm(eip + 1, &rm);
+
+	imm.type = OPR_IMM;
+	imm.addr = eip + len;
+
+	operand_read(&imm);
+	operand_read(&rm);
+	int iv = sign_ext(imm.val, 8);
+	rm.val = alu_add(iv, rm.val, data_size);
+	operand_write(&rm);
+	return 1 + len;
+}
