5085a90ed35976db4f7a4185bdcb5a5eaece2690
tracer-pa2018-fall <tracer@njuics.org>
2018-10-02 09:31:09 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:31:09 up 1 day, 17:45,  1 user,  load average: 0.17, 0.10, 0.05 bd1b71e98a9d7a5825c5e63d111acf1dbbd6748

--- nemu/src/cpu/instr/alu_instr.c
+++ nemu/src/cpu/instr/alu_instr.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op_add(){
+static void instr_execute_2op(){
 	operand_read(&opr_src);
 	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
 	operand_write(&opr_dest);
