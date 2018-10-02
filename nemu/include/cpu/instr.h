48fa423c9b2e81d3ece4c53f83df859faec40ab0
monitor <tracer@njuics.org>
2018-10-02 11:48:01 +0800

    monitor test

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -26,8 +26,8 @@ extern uint8_t data_size;
 make_instr_func(push_r_v);
 
 make_instr_func(cmp_i2rm_bv);
-make_instr_func(jmp_near);
-make_instr_func(jmp_short);
+
+make_instr_func(lea);
 
 make_instr_func(call_near);
 make_instr_func(ret_near);
