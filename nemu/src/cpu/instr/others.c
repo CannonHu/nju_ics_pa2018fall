3ebfd596aea305fa91dbb76310c72febbd578ab1
monitor <tracer@njuics.org>
2018-10-02 13:40:01 +0800

    monitor test

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -21,26 +21,6 @@ make_instr_func(push_r_v){
 }
 
 
-
-make_instr_func(cmp_i2rm_bv){
-	OPERAND rm, imm;
-	rm.data_size = data_size;
-	imm.data_size = 8;
-
-	int len = 1;
-	len += modrm_rm(eip + 1,&rm);
-
-	imm.type = OPR_IMM;
-	imm.addr = eip + len;
-	operand_read(&imm);
-	operand_read(&rm);
-
-	uint32_t tval = sign_ext(imm.val, 8);
-	alu_sub(tval, rm.val, data_size);
-	return 1 + len;
-		
-}
-
 make_instr_func(call_near){
 	uint32_t eipval = 0;
 	OPERAND dest,addr;
