020b424385438dd36501ef5d0c64bbfaae310b41
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 14:17:15 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:17:15 up 2 days,  6:10,  1 user,  load average: 0.18, 0.08, 0.02 5f3877f24135c7f85d6bb4b54fe729b975d4f37d

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -3,7 +3,9 @@
 static void instr_execute_2op(){
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
+	printf("src: %x dest: %x",opr_src.val, opr_dest.val);
 	opr_dest.val = alu_xor(opr_src.val, opr_dest.val, data_size);
+	printf(" res: %x\n",opr_dest.val);
 	operand_write(&opr_dest);
 }
 
