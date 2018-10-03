df8213e6d9f993e21cebc7441dd83d3450cfee4a
monitor <tracer@njuics.org>
2018-10-03 22:48:01 +0800

    monitor test

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -170,15 +170,6 @@ make_instr_func(ret_near_imm16){
 	return 0;
 }
 
-static void instr_execute_2op(){
-	operand_read(&opr_src);
-	operand_read(&opr_dest);
-	alu_and(opr_src.val, opr_dest.val, data_size);
-}
-
-make_instr_impl_2op(test, r, rm, b);
-make_instr_impl_2op(test, r, rm, v);
-make_instr_impl_2op(test, i, rm, v);
 
 make_instr_func(lea){
 	OPERAND r, rm;
