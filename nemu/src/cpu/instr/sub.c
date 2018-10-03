3509cef27f9218948432e34573e337a37210a8f8
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 17:31:40 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  17:31:40 up 2 days,  9:16,  1 user,  load average: 0.11, 0.10, 0.07 46fa680cf1c5b4d0560be9929df111c0374745b6

--- nemu/src/cpu/instr/sub.c
+++ nemu/src/cpu/instr/sub.c
@@ -1,11 +1,13 @@
 #include "cpu/instr.h"
 
-/*static void instr_execute_2op(){
+static void instr_execute_2op(){
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest = alu_sub(opr_src.val, opr_dest.val ,data_size);
 	operand_write(&opr_dest);
-}*/
+}
+
+make_instr_impl_2op(sub, rm, r, v);
 
 make_instr_func(sub_i2rm_bv){
 	OPERAND rm, imm;
