88790272fce6d9565ffc7ef4d7003380c997172d
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 19:43:54 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:43:54 up 2 days, 10:06,  1 user,  load average: 0.19, 0.19, 0.16 2ed24ebd5a9c5487e362c3f74e7e22f378d890af

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -47,6 +47,7 @@ make_instr_func(add_i2rm_bv);
 make_instr_func(adc_r2rm_v);
 
 make_instr_func(sub_i2rm_bv);
+make_instr_func(sub_r2rm_v);
 make_instr_func(sub_rm2r_v);
 
 make_instr_func(and_i2rm_bv);
