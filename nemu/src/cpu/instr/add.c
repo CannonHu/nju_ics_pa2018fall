2a84cc662e2574ffbb92dd3cd87cb17f28a63eb4
monitor <tracer@njuics.org>
2018-10-03 14:34:01 +0800

    monitor test

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -3,9 +3,7 @@
 static void instr_execute_2op(){
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	printf("src: %x dest: %x",opr_src.val, opr_dest.val);
 	opr_dest.val = alu_add(opr_src.val, opr_dest.val, data_size);
-	printf("res: %x\n", opr_dest.val);
 	operand_write(&opr_dest);
 }
 
