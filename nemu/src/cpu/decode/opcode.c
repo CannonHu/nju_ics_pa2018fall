015e904cf5dfe2b0dc4446b00d378e231d7fcf7e
monitor <tracer@njuics.org>
2018-10-03 14:44:01 +0800

    monitor test

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -21,7 +21,7 @@ instr_func opcode_entry[256] = {
 /* 0x44 - 0x47*/	inv, inv, inv, inv,
 /* 0x48 - 0x4b*/	inv, inv, inv, inv,
 /* 0x4c - 0x4f*/	inv, inv, inv, inv,
-/* 0x50 - 0x53*/	push_r_v, push_r_v, push_r_v, push_r_v,
+/* 0x50 - 0x53*/	push_r_v, __ref_push_r_v, push_r_v, push_r_v,
 /* 0x54 - 0x57*/	push_r_v, push_r_v, push_r_v, push_r_v,
 /* 0x58 - 0x5b*/	inv, inv, inv, pop_r_v,
 /* 0x5c - 0x5f*/	pop_r_v, pop_r_v, inv, inv,
@@ -77,7 +77,7 @@ instr_func group_1_v_entry[8] =
 
 /* 0x83 */
 instr_func group_1_bv_entry[8] =
-{add_i2rm_bv, inv, inv, inv, and_i2rm_bv, sub_i2rm_bv, inv, cmp_i2rm_bv};
+{add_i2rm_bv, inv, inv, inv, __ref_and_i2rm_bv, sub_i2rm_bv, inv, cmp_i2rm_bv};
 
 /* 0xc0 */
 instr_func group_2_b_entry[8] =
@@ -113,7 +113,7 @@ instr_func group_3_v_entry[8] =
 
 /* 0xff */
 instr_func group_5_indirect_entry[8] =
-{inc_rm_v, inv, inv, inv, inv, inv, push_rm_v, inv};
+{inc_rm_v, inv, inv, inv, inv, inv, __ref_push_rm_v, inv};
 
 instr_func group_7_entry[8] = 
 {inv, inv, inv, inv, inv, inv, inv, inv};
