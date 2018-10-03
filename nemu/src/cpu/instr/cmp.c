2352e6a2b069554d7597f0c68e0cc1b4ff89a3f4
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 22:42:30 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  22:42:30 up 2 days, 13:05,  1 user,  load average: 0.25, 0.15, 0.05 8a8d8e8d5f4a075d6baa2f537453fbce48a13cf

--- nemu/src/cpu/instr/cmp.c
+++ nemu/src/cpu/instr/cmp.c
@@ -26,6 +26,7 @@ make_instr_func(cmp_i2rm_bv){
 
 make_instr_impl_2op(cmp, i, rm, v);
 make_instr_impl_2op(cmp, r, rm, v);
+make_instr_impl_2op(cmp, rm, r, b);
 make_instr_impl_2op(cmp, rm, r, v);
 make_instr_impl_2op(cmp, i, a, b);
 make_instr_impl_2op(cmp, i, rm ,b);
