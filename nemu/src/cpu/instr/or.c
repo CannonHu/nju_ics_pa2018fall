5049b5ea40152aa9db49fff181081a78ea7c741f
monitor <tracer@njuics.org>
2018-10-03 09:31:02 +0800

    monitor test

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -1,1 +1,10 @@
+#include "cpu/instr.h"
 
+static void instr_execute_2op(){
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_or(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
+
+make_instr_impl_2op(xor, r, rm, v);
