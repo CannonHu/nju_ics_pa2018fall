d47d094bb70854fd0ea3b9463eedbd68dd371a5b
monitor <tracer@njuics.org>
2018-10-03 09:21:02 +0800

    monitor test

---
+++ nemu/src/cpu/instr/xor.c
@@ -0,0 +1,8 @@
+#include "cpu/instr.h"
+
+static void instr_execute_2op(){
+	operand_read(&opr_src);
+	operand_read(&opr_dest);
+	opr_dest.val = alu_xor(opr_src.val, opr_dest.val, data_size);
+	operand_write(&opr_dest);
+}
