0dfe8aaacc592d5e1a855ecba8eb85308fd99ce4
tracer-pa2018-fall <tracer@njuics.org>
2018-10-03 09:51:13 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  09:51:13 up 2 days,  5:05,  1 user,  load average: 0.13, 0.18, 0.13 51f0b319d33fe5e0b9bb34508310b6d7eaddd4ff

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -4,6 +4,7 @@ static void instr_execute_2op(){
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
 	opr_dest.val = alu_or(opr_src.val, opr_dest.val, data_size);
+	printf("src %x dest %x\n",opr_src.val, opr_dest.val);
 	operand_write(&opr_dest);
 }
 
