f8a65d7f5bf39bc05c7ae65eb55c94c69b346261
monitor <tracer@njuics.org>
2018-10-04 09:01:01 +0800

    monitor test

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -17,6 +17,26 @@ make_instr_func(jmp_near) {
         return 1 + data_size / 8;
 }
 
+make_instr_func(jmp_near_indirect) {
+        OPERAND rel;
+        rel.type = OPR_IMM;
+	rel.sreg = SREG_CS;
+        rel.data_size = data_size;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+
+	int offset = sign_ext(rel.val, data_size);
+	print_asm_1("jmp", "", 2, &rel);
+
+	cpu.eip += offset;
+
+	if(data_size == 16)
+		cpu.eip &= 0xffff;
+
+        return 1 + data_size / 8;
+}
+
 make_instr_func(jmp_short){
         OPERAND rel;
         rel.type = OPR_IMM;
