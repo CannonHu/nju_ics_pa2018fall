e178d71ce656559b47da3f914b15b5008e83d4b0
tracer-pa2018-fall <tracer@njuics.org>
2018-10-08 14:56:13 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:56:13 up 44 min,  1 user,  load average: 0.25, 0.13, 0.13 1ac1f2b7dd88346bb72b04cda1f3759188f2af8

--- nemu/src/cpu/instr/xor.c
+++ nemu/src/cpu/instr/xor.c
@@ -9,3 +9,7 @@ static void instr_execute_2op(){
 
 make_instr_impl_2op(xor, r, rm, b);
 make_instr_impl_2op(xor, r, rm, v);
+make_instr_impl_2op(xor, rm, r, b);
+make_instr_impl_2op(xor, rm, r, v);
+make_instr_impl_2op(xor, i, a, b);
+make_instr_impl_2op(xor, i, a, v);
