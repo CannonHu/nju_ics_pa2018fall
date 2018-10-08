082fcdd6175e1c3494966960be4fe303e9c26cba
monitor <tracer@njuics.org>
2018-10-08 14:58:01 +0800

    monitor test

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -31,10 +31,12 @@ make_instr_func(pop_r_v);
 make_instr_func(cmp_i2rm_v);
 make_instr_func(cmp_i2rm_b);
 make_instr_func(cmp_i2rm_bv);
+make_instr_func(cmp_r2rm_b);
 make_instr_func(cmp_r2rm_v);
 make_instr_func(cmp_rm2r_b);
 make_instr_func(cmp_rm2r_v);
 make_instr_func(cmp_i2a_b);
+make_instr_func(cmp_i2a_v);
 
 make_instr_func(lea);
 make_instr_func(leave);
