65cf5f7df1aad9a3983b8651ba51b8949cf415e0
monitor <tracer@njuics.org>
2018-10-08 14:35:01 +0800

    monitor test

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -11,3 +11,5 @@ make_instr_impl_2op(adc, r, rm, b);
 make_instr_impl_2op(adc, r, rm, v);
 make_instr_impl_2op(adc, rm, r, b);
 make_instr_impl_2op(adc, rm, r, v);
+make_instr_impl_2op(adc, i, a, b);
+make_instr_impl_2op(adc, i, a, v);
