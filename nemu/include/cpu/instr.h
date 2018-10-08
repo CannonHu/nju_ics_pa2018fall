a50f87cf8d414eff2d9d7d3f1b994103d8fed8f3
tracer-pa2018-fall <tracer@njuics.org>
2018-10-08 14:09:36 +0800

    >  clean 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  14:09:36 up 50 min,  1 user,  load average: 0.01, 0.19, 0.15 cd53c60e6ff81bd6475c4b1d58e0560f3d70f7ad

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -72,6 +72,9 @@ make_instr_func(or_r2rm_b);
 make_instr_func(or_r2rm_v);
 make_instr_func(or_rm2r_b);
 make_instr_func(or_rm2r_v);
+make_instr_func(or_i2a_b);
+make_instr_func(or_i2a_v);
+
 
 make_instr_func(xor_r2rm_v);
 
--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -11,3 +11,5 @@ make_instr_impl_2op(or, r, rm, b);
 make_instr_impl_2op(or, r, rm, v);
 make_instr_impl_2op(or, rm, r, b);
 make_instr_impl_2op(or, rm, r, v);
+make_instr_impl_2op(or, i, a, b);
+make_instr_impl_2op(or, i, a, v);
