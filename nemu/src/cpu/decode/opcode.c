87b7f02545555aa84c59d27f6afe120067138e9b
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 21:33:16 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:33:16 up  4:25,  1 user,  load average: 0.21, 0.13, 0.14 799b3c733c2014fef0220d8a1e284cea26949b0f

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -61,7 +61,7 @@ instr_func opcode_entry[256] = {
 /* 0xe4 - 0xe7*/	inv, inv, inv, inv,
 /* 0xe8 - 0xeb*/	call_near,jmp_near, inv, jmp_short,
 /* 0xec - 0xef*/	inv, inv, inv, inv,
-/* 0xf0 - 0xf3*/	inv, break_point, inv, rep_repe,
+/* 0xf0 - 0xf3*/	inv, break_point, inv, __ref_rep_repe,
 /* 0xf4 - 0xf7*/	hlt, inv, group_3_b, group_3_v,
 /* 0xf8 - 0xfb*/	clc, inv, inv, inv,
 /* 0xfc - 0xff*/	cld, inv, inv, group_5_indirect,
