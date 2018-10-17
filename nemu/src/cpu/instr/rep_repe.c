f7b06ffc5d4b0bfad94426584494026d7171dd90
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 21:40:22 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  21:40:22 up  4:32,  1 user,  load average: 0.14, 0.14, 0.14 ea105e8a311a69e969621b107fe60e3315c5bc5e

--- nemu/src/cpu/instr/rep_repe.c
+++ nemu/src/cpu/instr/rep_repe.c
@@ -4,6 +4,7 @@ make_instr_func(rep_repe) {
 	int len = 1;
 	uint8_t op = instr_fetch(eip + 1, 1);
 	print_asm_0("rep", "", len + 1);
+	printf("op: %x\n",op);
 	if(op == 0xc3) {
 		// reference http://repzret.org/p/repzret/
 		opcode_entry[op](eip + 1, op);
