0965a9a0e38b716ee53410afa76ee9bd402bcdd6
tracer-pa2018-fall <tracer@njuics.org>
2018-10-17 13:45:42 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:45:42 up 59 min,  1 user,  load average: 0.22, 0.24, 0.14 aa0cdffc8791be4dc2ee75f8cb9d7ef313e06d80

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,7 +33,7 @@ uint32_t loader() {
 	eph = ph + elf->e_phnum;
 	for(; ph < eph; ph ++) {
 		if(ph->p_type == PT_LOAD) {
-			printf("shepiguai\n");	
+
 			memcpy((void*)ph->p_vaddr, (void*)ph->p_offset, ph->p_filesz);	
 			/* TODO: copy the segment from the ELF file to its proper memory area */
 			memcpy((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz);		
