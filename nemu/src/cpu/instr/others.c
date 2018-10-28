9e79e382c6a1f70693c60eacc168fa716da57501
monitor <tracer@njuics.org>
2018-10-28 19:22:01 +0800

    monitor test

--- nemu/src/cpu/instr/others.c
+++ nemu/src/cpu/instr/others.c
@@ -352,17 +352,3 @@ make_instr_func(leave){
 	return 1;
 }
 
-make_instr_func(not_rm_v){
-	OPERAND rm;
-	rm.data_size = data_size;
-
-	int len = 1;
-	len += modrm_rm(eip + 1, &rm);
-
-	operand_read(&rm);
-
-	print_asm_1("not","",len, &rm);
-	rm.val = ~rm.val;
-	operand_write(&rm);
-	return len;
-}
