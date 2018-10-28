1f5b9cd4ebd9d0a68b35a994953725d847487522
monitor <tracer@njuics.org>
2018-10-28 19:11:01 +0800

    monitor test

--- nemu/src/cpu/instr/or.c
+++ nemu/src/cpu/instr/or.c
@@ -16,3 +16,21 @@ make_instr_impl_2op(or, i, a, v);
 make_instr_impl_2op(or, i, rm, b);
 make_instr_impl_2op(or, i, rm, v);
 
+make_instr_func(or_i2rm_bv){
+	OPERAND rm, imm;
+	rm.data_size = imm.data_size = data_size;
+
+	int len = 1;
+	len += modrm_rm(eip + 1, &rm);
+
+	imm.type = OPR_IMM;
+	imm.addr = eip + len;
+
+	operand_read(&imm);
+	operand_read(&rm);
+	print_asm_2("or","bv",len, &imm, &rm);
+	int iv = sign_ext(imm.val, 8);
+	rm.val = alu_or(iv, rm.val, data_size);
+	operand_write(&rm);
+	return 1 + len;
+}
