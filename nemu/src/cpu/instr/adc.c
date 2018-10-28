eb1a4649593ff5be165e662bc3fdecb4639dd571
monitor <tracer@njuics.org>
2018-10-28 19:13:01 +0800

    monitor test

--- nemu/src/cpu/instr/adc.c
+++ nemu/src/cpu/instr/adc.c
@@ -15,3 +15,22 @@ make_instr_impl_2op(adc, i, a, b);
 make_instr_impl_2op(adc, i, a, v);
 make_instr_impl_2op(adc, i, rm, b);
 make_instr_impl_2op(adc, i, rm, v);
+
+make_instr_func(adc_i2rm_bv){
+	OPERAND rm, imm;
+	rm.data_size = imm.data_size = data_size;
+
+	int len = 1;
+	len += modrm_rm(eip + 1, &rm);
+
+	imm.type = OPR_IMM;
+	imm.addr = eip + len;
+
+	operand_read(&imm);
+	operand_read(&rm);
+	print_asm_2("adc","bv",len, &imm, &rm);
+	int iv = sign_ext(imm.val, 8);
+	rm.val = alu_adc(iv, rm.val, data_size);
+	operand_write(&rm);
+	return 1 + len;
+}
