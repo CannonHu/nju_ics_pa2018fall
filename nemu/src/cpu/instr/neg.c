6027ef0b2c2da7bd8c7ead0aa65b8b01961a931e
tracer-pa2018-fall <tracer@njuics.org>
2018-10-28 19:38:57 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:38:57 up 3 days,  9:05,  1 user,  load average: 0.51, 0.31, 0.26 19a77e5fa2aaf7b783b78f754fe16db37e37f053

--- nemu/src/cpu/instr/neg.c
+++ nemu/src/cpu/instr/neg.c
@@ -2,7 +2,7 @@
 
 static void instr_execute_1op(){
 	operand_read(&opr_src);
-	opr_src.val = alu_sub(opr_src, 0, data_size);
+	opr_src.val = alu_sub(opr_src.val, 0, data_size);
 	if(opr_src.val == 0){
 		cpu.eflags.CF = 0;
 	}
