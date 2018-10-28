7c332fbd73d08ac8c481cfbdbd7b0fe3e56e7b08
monitor <tracer@njuics.org>
2018-10-28 19:38:01 +0800

    monitor test

---
+++ nemu/src/cpu/instr/neg.c
@@ -0,0 +1,16 @@
+#include "cpu/instr.h"
+
+static void instr_execute_1op(){
+	operand_read(&opr_src);
+	opr_src.val = alu_sub(opr_src, 0, data_size);
+	if(opr_src.val == 0){
+		cpu.eflags.CF = 0;
+	}
+	else{
+		cpu.eflags.CF = 1;
+	}
+	operand_write(&opr_src);
+}
+
+make_instr_impl_1op(not, rm, b);
+make_instr_impl_1op(not, rm, v);
