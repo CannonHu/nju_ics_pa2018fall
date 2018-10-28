58427f1f77750a8fa3637381f37bddf235367400
monitor <tracer@njuics.org>
2018-10-28 19:08:01 +0800

    monitor test

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -115,6 +115,8 @@ make_instr_func(xor_rm2r_b);
 make_instr_func(xor_rm2r_v);
 make_instr_func(xor_i2a_b);
 make_instr_func(xor_i2a_v);
+make_instr_func(xor_i2rm_b);
+make_instr_func(xor_i2rm_v);
 
 make_instr_func(not_rm_v);
 
