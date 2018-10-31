4cb95c5d4c0f12500782521a5da6742cd474594d
monitor <tracer@njuics.org>
2018-10-31 21:38:01 +0800

    monitor test

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -219,7 +219,6 @@ int eval(int p, int q){
 			case '*':return val1 * val2;
 			case '/':return val1 / val2;
 			case '%':return val1 % val2;
-			case NEG:return val1 - val2;
 			default:assert(0);
 		}
 	}
