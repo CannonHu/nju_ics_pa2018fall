69d3fd5c70cb5b2a6d83107be9803fd0538f578a
tracer-pa2018-fall <tracer@njuics.org>
2018-11-01 16:12:52 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  16:12:52 up 4 days, 13:15,  1 user,  load average: 0.34, 0.16, 0.11 5b58544381a931dac1e9567011ccb7dcfd7c8d9e

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -223,6 +223,7 @@ int tokentoval(int id){
 }
 
 int eval(int p, int q){
+	uint32_t vaddr;
 	if(p > q){
 		printf("BAD\n");
 		return 0;
@@ -243,8 +244,8 @@ int eval(int p, int q){
 			case '%':return eval(p, opnum - 1) % eval(opnum + 1, q);
 			case NEG:return 0 - eval(opnum + 1,q);
 			case DEREF: 
-				 uint32_t addr = eval(opnum + 1,q);
-				 return vaddr_read(addr, SREG_CS, 4);
+				 vaddr = eval(opnum + 1,q);
+				 return vaddr_read(vaddr, SREG_CS, 4);
 			default:assert(0);
 		}	
 	}
