d7677e1ffac0d45f0a60af3ad05e27bae68749c9
monitor <tracer@njuics.org>
2018-11-02 20:54:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -1,12 +1,13 @@
 #include "nemu.h"
 
+
 typedef struct{
 	bool valid;
 	uint32_t sign;
 	uint8_t data_cell[64];
 }CacheLine;
 
-init_cache();
+void init_cache();
 
 uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
 
