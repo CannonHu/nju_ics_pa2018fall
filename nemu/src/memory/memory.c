e7e30540aa2eaa6b8b68c82468c1dfbb413ccacc
monitor <tracer@njuics.org>
2018-11-03 12:28:01 +0800

    monitor test

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,6 +1,7 @@
 #include "nemu.h"
 #include "cpu/cpu.h"
 #include "memory/memory.h"
+#include "memory/cache.h"
 #include "device/mm_io.h"
 #include <memory.h>
 #include <stdio.h>
@@ -20,12 +21,21 @@ void hw_mem_write(paddr_t paddr, size_t len, uint32_t data) {
 
 uint32_t paddr_read(paddr_t paddr, size_t len) {
 	uint32_t ret = 0;
+#ifdef CACHE_ENABLED
+	ret = cache_read(paddr, len, cache);
+#else
 	ret = hw_mem_read(paddr, len);
+#endif
 	return ret;
 }
 
 void paddr_write(paddr_t paddr, size_t len, uint32_t data) {
+#ifdef CACHE_ENABLED
+	cache_write(paddr, len, data, cache);
+#else
 	hw_mem_write(paddr, len, data);
+#endif
+
 }
 
 
