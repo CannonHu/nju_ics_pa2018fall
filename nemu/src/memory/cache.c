fb7fa599987c796563c02a8c127267576c5fbe04
tracer-pa2018-fall <tracer@njuics.org>
2018-11-03 13:10:21 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:10:21 up 5 days,  7:22,  1 user,  load average: 0.13, 0.16, 0.20 11dac4b0818b2aa0fcb9b9e59957c8220634b

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -77,9 +77,9 @@ uint32_t cache_read(paddr_t paddr, size_t len){
 		addrn = (paddr & 0xffffffc0) + 0x40;
 		slot_id ++;
 		ret += cache_read_line(addrn, slot_id, line_sign, seclen) << (firstlen * 8);
-		return ret;
-
-	}	
+	}
+	printf("paddr read res: %x\n", ret);
+	return ret;
 
 }
 
