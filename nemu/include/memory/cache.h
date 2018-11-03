83587b767e4813dc98ad4cedbec252b56f42a4b1
monitor <tracer@njuics.org>
2018-11-03 13:04:01 +0800

    monitor test

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -15,6 +15,6 @@ CacheLine cache[SLOT_NUM][LINE_IN_SLOT];
 
 void init_cache();
 
-uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache);
+uint32_t cache_read(paddr_t paddr, size_t len);
 
-void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache);
+void cache_write(paddr_t paddr, size_t len, uint32_t data);
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -60,7 +60,7 @@ uint32_t cache_read_line(paddr_t paddr, uint8_t slot_id, uint32_t line_sign, siz
 	return ret;
 }
 
-uint32_t cache_read(paddr_t paddr, size_t len, CacheLine* cache){
+uint32_t cache_read(paddr_t paddr, size_t len){
 	printf("paddr: %x\n", paddr);
 	uint32_t ret = 0;
 	paddr_t addrn = paddr;
@@ -96,7 +96,7 @@ void cache_write_line(paddr_t paddr, uint8_t slot_id, uint32_t line_sign, uint32
 }
 
 
-void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache){
+void cache_write(paddr_t paddr, size_t len, uint32_t data){
 	paddr_t addrn = paddr;
 	uint32_t slot_id = get_slot(addrn);
 	uint32_t line_sign = get_line_sign(addrn);
