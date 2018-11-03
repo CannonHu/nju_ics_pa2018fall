4a5df9f95c1e2cb3fb79517cff9c4a613af4578d
tracer-pa2018-fall <tracer@njuics.org>
2018-11-03 12:31:54 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:31:54 up 5 days,  6:44,  1 user,  load average: 0.34, 0.23, 0.13 3f3b0c85164aff6ea37932f878e0937655be399b

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -107,7 +107,7 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine* cache){
 		cache_write_line(addrn, slot_id, line_sign, data, len);
 	}
 	else{
-		int firstlen = line_data_size - cell_num;i
+		int firstlen = line_data_size - cell_num;
 		int seclen = len - firstlen;
 		cache_write_line(addrn, slot_id, line_sign, data, firstlen);
 		addrn = (paddr & 0xffffffc0) + 0x40;
