aefda278545a10d13a510cad673179cb35ffb6b0
monitor <tracer@njuics.org>
2018-11-11 10:06:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -14,13 +14,20 @@ void load_sreg(uint8_t sreg) {
 	/* TODO: load the invisibile part of the segment register 'sreg' by reading the GDT.
 	 * The visible part of 'sreg' should be assigned by mov or ljmp already.
 	 */
-	uint32_t segDesc_addr = (cpu.segReg[sreg].index * 64) + cpu.gdtr.base;
-	SegDesc* sdt = segDesc_addr;
-	cpu.segReg[sreg].base = (sdt->base_15_0) | (sdt->base_23_16 << 16) | (sdt->base_31_24 << 24);
-	cpu.segReg[sreg].limit = (sdt->limit_15_0) | (sdt->limit_19_16 << 16); 
-	0cpu.segReg[sreg].type = sdt->type;
-	cpu.segReg[sreg].privilege_level = sdt->privilege_level;
-	cpu.segReg[sreg].soft_use = sdt->soft_use;
+	assert(cpu.segReg[sreg].ti == 0);
 
+	if(cpu.segRef[sreg].ti == 0){
+		uint32_t segDesc_addr = (cpu.segReg[sreg].index * 64) + cpu.gdtr.base;
+		SegDesc* sdt = segDesc_addr;
+		
+		assert(sdt->present == 1);
+		assert(sdt->granularity == 1);
+
+		cpu.segReg[sreg].base = (sdt->base_15_0) | (sdt->base_23_16 << 16) | (sdt->base_31_24 << 24);
+		cpu.segReg[sreg].limit = (sdt->limit_15_0) | (sdt->limit_19_16 << 16); 
+		cpu.segReg[sreg].type = sdt->type;
+		cpu.segReg[sreg].privilege_level = sdt->privilege_level;
+		cpu.segReg[sreg].soft_use = sdt->soft_use;
+	}
 }
 
