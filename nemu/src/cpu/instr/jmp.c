be9e8d15a63514bc2366ecfaa537fedc9aee3a53
tracer-pa2018-fall <tracer@njuics.org>
2018-11-12 15:26:26 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:26:26 up 8 days,  1:55,  1 user,  load average: 0.11, 0.21, 0.11 48419cf5da506d1cff9d44f5ac0d60ad70c5a6dd

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -83,6 +83,7 @@ make_instr_func(jmp_far_imm){
 	{
 		uint32_t addr = cpu.gdtr.base + ((ptr_sec.val >> 3) & 0xffff) * 8;
 		printf("addr: %x\n", addr);
+		printf("%x\n",ptr_sec.val);
 		SegDesc* sdt = (SegDesc*)addr;
 
 		assert(sdt->present == 1);
