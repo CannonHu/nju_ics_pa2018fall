9264d5ecbbf94a5f625995a7145d12f83debac50
monitor <tracer@njuics.org>
2018-11-12 16:42:01 +0800

    monitor test

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -18,7 +18,9 @@ void load_sreg(uint8_t sreg) {
 
 	if(cpu.segReg[sreg].ti == 0){
 		uint32_t segDesc_addr = (cpu.segReg[sreg].index * 8) + cpu.gdtr.base;
-		SegDesc sdt = (SegDesc*)segDesc_addr;
+		SegDesc sdt;
+		sdt.val[0] = vaddr(segDesc_addr, SREG_CS, 4);
+		sdt.val[1] = vaddr(segDesc_addr + 4, SREG_CS, 4);
 		
 		assert(sdt.present == 1);
 		assert(sdt.granularity == 1);
