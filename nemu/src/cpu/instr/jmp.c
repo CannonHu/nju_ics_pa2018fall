8b37e92bbfed129378278915e4490cc240c79eb0
tracer-pa2018-fall <tracer@njuics.org>
2018-11-12 15:18:27 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:18:27 up 8 days,  1:47,  1 user,  load average: 0.29, 0.14, 0.04 885a2918385f88a3691e8fb84654ca70b2b7f42f

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -63,6 +63,8 @@ make_instr_func(jmp_far_imm){
 	ptr_sec.type = ptr_off.type = OPR_IMM;
 	ptr_sec.data_size = 16;
 	ptr_off.data_size = data_size;
+
+	printf("reached\n");
 	
 	ptr_sec.addr = eip + 1;
 	operand_read(&ptr_sec);
