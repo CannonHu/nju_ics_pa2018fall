41703f2400bd0c69dde3f175ccf3ab40607ab2bc
tracer-pa2018-fall <tracer@njuics.org>
2018-11-21 12:43:20 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:43:20 up 1 day,  7:01,  1 user,  load average: 0.18, 0.14, 0.06 ee938d698cd87d8eab49b4d1d5da1abc7031e8

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -9,7 +9,10 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg) {
 
 	//printf("offset: %x\n", offset);	
 	//assert(offset <= cpu.segReg[sreg].limit);
-	
+	if(offset > cpu.segReg[sreg].limit){
+		printf("Warning! offset > limit\n");
+	}
+
 	uint32_t laddr = cpu.segReg[sreg].base + offset;
 	return laddr;
 }
