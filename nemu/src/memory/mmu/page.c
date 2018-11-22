8419058d504e76bcbd6722ad487bcd3dbd24bfc6
tracer-pa2018-fall <tracer@njuics.org>
2018-11-23 00:09:47 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:09:47 up 2 days,  4:56,  1 user,  load average: 0.43, 0.40, 0.23 857bcfd9d98332a9629a7542d5ff02312c87c397

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -4,8 +4,9 @@
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
-	printf("used page!\n");
+	printf("used page! %x\n", laddr);
 	uint32_t pde_addr = (cpu.cr3.pdbr << 12) + (laddr >> 22);
+	printf("pde: %x\n", pde_addr);
 	PDE cur_pde;
        	cur_pde.val = paddr_read(pde_addr, 4);
 	assert(cur_pde.present == 1);
