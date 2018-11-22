9384a8f32848a67d81d124671e3ba65992876ea2
monitor <tracer@njuics.org>
2018-11-22 23:29:01 +0800

    monitor test

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -4,8 +4,11 @@
 // translate from linear address to physical address
 paddr_t page_translate(laddr_t laddr) {
 #ifndef TLB_ENABLED
-	printf("\nPlease implement page_translate()\n");
-	assert(0);
+	uint32_t pde_addr = (cpu.cr3.pdbr << 12) + (laddr >> 22);
+	PDE cur_pde = paddr_read(pde_addr, 4);
+	uint32_t pte_addr = (cur_pde << 12) + (laddr << 10 >> 22);
+	PTE cur_pte = paddr_read(pte_addr, 4);
+	return (cur_pte << 12) + (laddr & 0xfff); 
 #else	
 	return tlb_read(laddr) | (laddr & PAGE_MASK);;
 #endif
