5c78303e9ca85bd6e0374a946add302ca600c1a1
tracer-pa2018-fall <tracer@njuics.org>
2018-11-23 00:24:44 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:24:44 up 2 days,  5:11,  1 user,  load average: 0.10, 0.16, 0.17 f19f3e248f15bfcdec99990a49b4794ff2d572b

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -37,12 +37,11 @@ void init_page(void) {
 	cr3.page_directory_base = ((uint32_t)pdir) >> 12;
 	write_cr3(cr3.val);
 
-	BREAK_POINT
-
 	/* set PG bit in CR0 to enable paging */
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
+	BREAK_POINT
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */
