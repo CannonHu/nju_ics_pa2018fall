4b8e72df4d9e15cb0a3c8ed5f2e53fd4c541607f
monitor <tracer@njuics.org>
2018-11-23 14:06:03 +0800

    monitor test

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -36,10 +36,10 @@ uint32_t loader() {
 			uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
 			Log("mm_malloc_addr: %x vaddr: %x\n", paddr, ph->p_vaddr);		
 			BREAK_POINT
-			memset((void*)ph->p_vaddr, 0, ph->p_memsz);
+			//memset((void*)ph->p_vaddr, 0, ph->p_memsz);
 			memcpy((void*)ph->p_vaddr, (void*)elf + ph->p_offset, ph->p_filesz);	
 			/* TODO: copy the segment from the ELF file to its proper memory area */
-			//memset((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);		
+			memset((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);		
 			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 
 #ifdef IA32_PAGE
