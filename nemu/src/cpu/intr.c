6ef0ff48d388b0ea30ec7dfbfbbec2d5a4ca3106
monitor <tracer@njuics.org>
2018-12-07 22:24:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -4,9 +4,17 @@
 
 
 void raise_intr(uint8_t intr_no) {
-#ifdef IA32_INTR
+#ifdef IA32_INTR/*
 	printf("Please implement raise_intr()");
-	assert(0);
+	assert(0);*/
+	OPERAND top;
+	top.type = OPR_MEM;
+	top.sreg = SREG_SS;
+	top.data_size = 32;
+	cpu.esp -= 4;
+	top.addr = esp;
+	top.val = 
+	
 #endif
 }
 
