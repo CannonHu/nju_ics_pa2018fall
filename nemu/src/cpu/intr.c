eff9ca8988b524bcaeaf07eda13fdfb7ad998f46
monitor <tracer@njuics.org>
2018-12-07 22:43:01 +0800

    monitor test

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,17 +11,21 @@ void raise_intr(uint8_t intr_no) {
 	top.type = OPR_MEM;
 	top.sreg = SREG_SS;
 	top.data_size = 32;
+
+	//push eflags
 	cpu.esp -= 4;
 	top.addr = cpu.esp;
 	top.val = cpu.eflags.val;
         operand_write(&top);
-	
+
+	//push sreg cs visible part
 	cpu.esp -= 2;
 	top.data_size = 16;
 	top.addr = cpu.esp;
 	top.val = cpu.cs.val;
 	operand_write(&top);
-
+	
+	//push sreg cs invisible part
 	cpu.esp -= 4;
 	top.data_size = 32;
 	top.addr = cpu.esp;
@@ -38,6 +42,13 @@ void raise_intr(uint8_t intr_no) {
 	top.addr = cpu.esp;
 	top.val = cpu.cs.type + cpu.cs.privilege_level << 5 + cpu.cs.soft_use << 7;
 	operand_write(&top);
+
+	cpu.esp -= 4;
+	top.addr = cpu.esp;
+	top.data_size = 32;
+	top.val = cpu.eip;
+	operand_write(&top);
+	
 	
 #endif
 }
