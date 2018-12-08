f4ff7fda932a9425066c4f820113cac4605385de
tracer-pa2018-fall <tracer@njuics.org>
2018-12-08 15:29:38 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  15:29:38 up 5 days, 11:26,  1 user,  load average: 0.00, 0.02, 0.02 f3678f8b11871519aa1654b560bcb4aa8ade465d

--- kernel/include/x86/memory.h
+++ kernel/include/x86/memory.h
@@ -102,6 +102,7 @@ typedef struct GateDescriptor {
 
 typedef struct TrapFrame {
 	uint32_t edi, esi, ebp, xxx, ebx, edx, ecx, eax;	// GPRs
+	uint32_t noname;
 	int32_t irq;										// #irq
 	uint32_t error_code;								// error code
 	uint32_t eip, cs, eflags;							// execution state saved by hardware
