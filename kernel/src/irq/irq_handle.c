5f51139ed8f23adb648fc8f02943ba749b0befce
monitor <tracer@njuics.org>
2018-12-08 14:41:02 +0800

    monitor test

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -36,7 +36,6 @@ void irq_handle(TrapFrame *tf) {
 	if (irq < 0) {
 		panic("Unhandled exception!");
 	} else if (irq == 0x80) {
-		BREAK_POINT
 		do_syscall(tf);
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
