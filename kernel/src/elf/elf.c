94a0017e2636706e0e222e0d398621018b859b2e
monitor <tracer@njuics.org>
2018-12-18 19:12:01 +0800

    monitor test

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -38,9 +38,13 @@ uint32_t loader() {
 			//Log("mm_malloc_addr: %x vaddr: %x\n", paddr, Eh->p_vaddr);		
 			
 			BREAK_POINT
-			//Log("filesize: %d", ph->p_filesz);
+			Log("filesize: %d", ph->p_filesz);
 #ifdef HAS_DEVICE_IDE
-			ide_read((void*)paddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+			//ide_read((void*)paddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
+			for(int i = 0; i < ph->p_filesz; i++){
+				uint8_t tmp = 0;
+				ide_read
+			}
 			//BREAK_POINT
 			//elf = (void*)buf;
 #else
