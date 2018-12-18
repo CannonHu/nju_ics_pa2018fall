6e6c3ff6b6e8517f3aed3c9e835e6e8a3b33f569
monitor <tracer@njuics.org>
2018-12-18 13:22:01 +0800

    monitor test

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -10,8 +10,21 @@ make_instr_func(in_i2r_b){
 	imm.type = OPR_IMM;
 	imm.addr = eip + 1;
 	
-	operamd_read(&imm);
+	operand_read(&imm);
 	r.val = pio_read(imm.val, 1);
 	operand_write(&r);
 	return 2;
+}
+
+make_instr_func(in_i2r_v){
+	OPERAND imm, r;
+	imm.data_size = 8;
+	r.data_size = data_size;
+	imm.sreg = SREG_CS;
+
+	r.type = OPR_REG;
+	r.addr = 0;
+	imm.type = OPR_IMM;
+	imm.addr = eip + 1;
+
 }
