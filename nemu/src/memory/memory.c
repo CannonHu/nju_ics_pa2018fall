caf9275a2048de311683a69a906072a015748c20
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 00:55:24 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:55:24 up 13:38,  1 user,  load average: 0.46, 0.78, 0.47 f1ddfe4af348f3e5cd5133231b259ac722c69a31

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -84,8 +84,8 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
 #ifdef IA32_PAGE
-	laddr_t paddr = laddr;/*
-	if(cpu.cr0.pg){
+	laddr_t paddr = laddr;
+	if(cpu.cr0.pg){/*
 		size_t len1 = ((paddr + len) >> 12 << 12) - paddr;
 		if(len1 != 0 && len1 < len){
 			printf("special twin-page: %x\n", laddr);
