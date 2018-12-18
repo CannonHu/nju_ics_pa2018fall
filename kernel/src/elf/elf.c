4193fd279e63d32e056c51d37a0577aef6191eb6
tracer-pa2018-fall <tracer@njuics.org>
2018-12-18 18:56:45 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  18:56:45 up  7:40,  1 user,  load average: 0.04, 0.01, 0.00 1629efbebc7fd92f1deda1c42a78a23ab2d88aaa

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -35,9 +35,9 @@ uint32_t loader() {
 	for(; ph < eph; ph ++) {
 		if(ph->p_type == PT_LOAD) {
 			uint32_t paddr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-			//Log("mm_malloc_addr: %x vaddr: %x\n", paddr, ph->p_vaddr);		
+			//Log("mm_malloc_addr: %x vaddr: %x\n", paddr, Eh->p_vaddr);		
 			
-			
+			BREAK_POINT
 #ifdef HAS_DEVICE_IDE
 			ide_read((void*)paddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
 			//BREAK_POINT
