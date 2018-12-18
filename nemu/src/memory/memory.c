5ed0d07252819d69ced5ce52554a4a9864a07959
monitor <tracer@njuics.org>
2018-12-19 00:25:01 +0800

    monitor test

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -84,7 +84,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
 	assert(len == 1 || len == 2 || len == 4);
-#ifdef IA32_PAGE
+#ifdef IA32_PAGE/*
 	laddr_t paddr = laddr;
 	if(cpu.cr0.pg){
 		size_t len1 = ((paddr + len) >> 12 << 12) - paddr;
@@ -108,7 +108,7 @@ void laddr_write(laddr_t laddr, size_t len, uint32_t data) {
 			paddr = page_translate(paddr);
 			return paddr_write(paddr, len, data);
 		}
-	}
+	}*/
 	paddr_write(paddr, len, data);
 
 #else
