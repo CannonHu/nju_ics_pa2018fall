bb8c10f7445df9d2154edb6d6d813f08c8b70673
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 00:14:00 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:14:00 up 12:57,  1 user,  load average: 0.05, 0.40, 0.37 b63a5b72b612a0ae5608decc3b92bae36ae6fc70

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -51,6 +51,9 @@ void video_mapping_read_test() {
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
 	for(i = 0; i < SCR_SIZE / 4; i ++) {
+		if(buf[i] != i){
+			printf("wrong i: %d", i);
+		}
 		assert(buf[i] == i);
 	}
 }
