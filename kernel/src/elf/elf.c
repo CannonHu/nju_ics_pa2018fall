014fd992a084e0a1780d2544130fcf08f1ab1605
monitor <tracer@njuics.org>
2018-12-18 15:55:01 +0800

    monitor test

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -37,7 +37,7 @@ uint32_t loader() {
 			//Log("mm_malloc_addr: %x vaddr: %x\n", paddr, ph->p_vaddr);		
 			
 			ide_read(buf, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
-			memcpy((void*)paddr, (void*)elf + ph->p_offset, ph->p_filesz);	
+			memcpy((void*)paddr, buf, ph->p_filesz);	
 			/* TODO: copy the segment from the ELF file to its proper memory area */
 			memset((void*)(paddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);		
 			/* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
