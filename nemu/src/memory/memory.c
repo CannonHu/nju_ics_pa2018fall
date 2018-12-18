eee499dc8f0d5a28b2ac64eead3444ca9ca178e7
tracer-pa2018-fall <tracer@njuics.org>
2018-12-18 19:41:19 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  19:41:19 up  8:24,  1 user,  load average: 0.10, 0.25, 0.41 4b2831da72a931d3243288123eb3afea12566918

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -48,7 +48,7 @@ uint32_t laddr_read(laddr_t laddr, size_t len) {
 	if(cpu.cr0.pg){
 		size_t len1 = ((paddr + len - 1) >> 12 << 12) - paddr;
 		if(len1 != 0 && len1 < len){
-			printf("special twin-page: %x\n", laddr);
+			//printf("special twin-page: %x\n", laddr);
 			uint32_t ret_val = 0;
 			paddr = page_translate(paddr);
 			ret_val = paddr_read(paddr, len1);
