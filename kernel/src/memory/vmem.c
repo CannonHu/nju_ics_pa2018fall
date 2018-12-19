f3667501e87943ada6e8e23cbab4a935ee6ba825
tracer-pa2018-fall <tracer@njuics.org>
2018-12-19 13:29:29 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  13:29:29 up 15:02,  1 user,  load average: 0.36, 0.30, 0.33 bdda423a55f7ed05110e5effa7048a2fe6febe9e

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -24,8 +24,7 @@ void create_video_mapping() {
 	pframe_idx = 0xa0000;
 	ptable_idx = 0xa0;
 
-	for(pdir_idx = 0; pdir_idx < SCR_SIZE/PT_SIZE; pdir_idx ++){
-		BREAK_POINT
+	for(pdir_idx = 0; pdir_idx < 2; pdir_idx ++){
 		pdir[pdir_idx].val = make_pde(ptable);
 		Log("pdir-val: %x", pdir[pdir_idx].val);
 		for(; ptable_idx < NR_PTE; ptable_idx++){
