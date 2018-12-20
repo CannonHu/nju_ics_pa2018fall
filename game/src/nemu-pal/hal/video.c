47d052f00486a8e10dd6fec9ffbf22d7c0794b41
monitor <tracer@njuics.org>
2018-12-20 13:10:01 +0800

    monitor test

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -11,12 +11,12 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect,
 		SDL_Surface *dst, SDL_Rect *dstrect) {
 	assert(dst && src);
 
-	//int sx = (srcrect == NULL ? 0 : srcrect->x);
-	//int sy = (srcrect == NULL ? 0 : srcrect->y);
-	//int dx = (dstrect == NULL ? 0 : dstrect->x);
-	//int dy = (dstrect == NULL ? 0 : dstrect->y);
-	//int w = (srcrect == NULL ? src->w : srcrect->w);
-	//int h = (srcrect == NULL ? src->h : srcrect->h);
+	int sx = (srcrect == NULL ? 0 : srcrect->x);
+	int sy = (srcrect == NULL ? 0 : srcrect->y);
+	int dx = (dstrect == NULL ? 0 : dstrect->x);
+	int dy = (dstrect == NULL ? 0 : dstrect->y);
+	int w = (srcrect == NULL ? src->w : srcrect->w);
+	int h = (srcrect == NULL ? src->h : srcrect->h);
 	//if(dst->w - dx < w) { w = dst->w - dx; }
 	//if(dst->h - dy < h) { h = dst->h - dy; }
 	//if(dstrect != NULL) {
