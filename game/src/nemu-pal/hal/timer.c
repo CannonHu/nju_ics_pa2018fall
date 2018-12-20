315ec57c0c52961184afe39a6a1b00418467cce9
monitor <tracer@njuics.org>
2018-12-20 23:29:01 +0800

    monitor test

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -25,8 +25,9 @@ timer_event(void) {
 
 uint32_t SDL_GetTicks() {
 	/* TODO: Return the time in millisecond. */
-	assert(0);
-	return 0;
+	//assert(0);
+	
+	return jiffy;
 }
 
 void SDL_Delay(uint32_t ms) {
