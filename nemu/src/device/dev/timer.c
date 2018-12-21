7e77b8cfeab30636c9bce270b8af1117d9d2cb5f
monitor <tracer@njuics.org>
2018-12-21 12:36:01 +0800

    monitor test

--- nemu/src/device/dev/timer.c
+++ nemu/src/device/dev/timer.c
@@ -24,7 +24,6 @@ static int TIMER_Thread(void* hz) {
 // start a timer with hz Hz
 void timer_start(int hz) {
 	timer_active = true;
-//	printf("hz: %x", hz);
 	SDL_CreateThread(TIMER_Thread, (void*) &hz);
 
 	SDL_Delay(10);
