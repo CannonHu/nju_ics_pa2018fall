ae93ac1b4cc12583220744a3602c3fb32ce187d5
tracer-pa2018-fall <tracer@njuics.org>
2018-12-21 12:21:52 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  12:21:52 up  4:45,  1 user,  load average: 0.19, 0.98, 1.08 d7587c87a15f22f832169c84215444ab9993085

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -70,10 +70,12 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
 		if(key_state[i] == KEY_STATE_PRESS) {
 			judge = true;
 			key_press_callback(keycode_hash[i]);
+			return true;
 		}
 		if(key_state[i] == KEY_STATE_RELEASE){
 			judge = true;
 			key_release_callback(keycode_hash[i]);
+			return false;
 		}
 			
 	}
