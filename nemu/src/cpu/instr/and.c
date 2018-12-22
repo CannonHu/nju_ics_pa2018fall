608a8552efdf762047b0c0ecd252b4c59f57e9bc
tracer-pa2018-fall <tracer@njuics.org>
2018-12-23 00:49:37 +0800

    >  nemu 151242017 shepiguai Linux debian 4.9.0-7-686-pae #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) i686 GNU/Linux  00:49:37 up 14:07,  1 user,  load average: 0.43, 1.07, 0.99 2792aefe989108515053570b21471c25dc81c115

--- nemu/src/cpu/instr/and.c
+++ nemu/src/cpu/instr/and.c
@@ -31,7 +31,7 @@ make_instr_func(and_i2rm_bv){
 	operand_read(&rm);
 	print_asm_2("and","bv",len, &imm, &rm);
 
-	int ival = imm.val;
+	int ival = imm.val & 0xff;;
 	rm.val = alu_and(ival, rm.val, data_size);
 	operand_write(&rm);
 	return 1 + len;
